0e5c067ebf7176e4b0cf712392ab77fc5caf1c8f
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2020-12-10 13:23:13 +0200

    Functions to check resources for 3 types of coffee are added.

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -10,38 +10,63 @@ public class CoffeeMachine {
     private static int cups = 9;
     private static int money = 550;
 
+    private static void checkResourcesForEspresso(int water, int milk, int beans, int cups) {
+
+        if (water >= 250 & beans >= 16 & cups >= 1) {
+
+            System.out.println("I have enough resources, making you a coffee!");
+        }
+
+    }
+
+    private static void checkResourcesForLatte(int water, int milk, int beans, int cups) {
+
+        if (water >= 350 & milk >= 75 & beans >= 20 & cups >= 1) {
+
+            System.out.println("I have enough resources, making you a coffee!");
+        }
+    }
+
+    private static void checkResourcesForCappuccino(int water, int milk, int beans, int cups) {
+
+        if (water >= 200 & milk >= 100 & beans >= 12 & cups >= 1) {
+
+            System.out.println("I have enough resources, making you a coffee!");
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Write action (buy, fill, take, remaining, exit): ");
 
         Scanner scanner = new Scanner(System.in);
 
+        String action = scanner.nextLine();
+
         System.out.println("The coffee machine has: \n" + water + " of water \n" + milk +
                 " of milk \n" + beans + " of coffee beans \n" + cups + " of disposable cups \n"
                 + money + " of money");
-
-        String action = scanner.nextLine();
-
-        System.out.println(action);
 
         switch (action) {
 
             case "buy":
 
-                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino");
 
                 int drinkChoice = scanner.nextInt();
 
-                System.out.println(drinkChoice);
+                //System.out.println(drinkChoice);
 
                 switch (drinkChoice) {
                     case 1:
+                        checkResourcesForEspresso(water, milk, beans, cups);
                         water = water - 250;
                         beans = beans - 16;
                         cups = cups - 1;
                         money = money + 4;
                         break;
                     case 2:
+                        checkResourcesForLatte(water, milk, beans, cups);
                         water = water - 350;
                         milk = milk - 75;
                         beans = beans - 20;
@@ -49,6 +74,7 @@ public class CoffeeMachine {
                         money = money + 7;
                         break;
                     case 3:
+                        checkResourcesForCappuccino(water, milk, beans, cups);
                         water = water - 200;
                         milk = milk - 100;
                         beans = beans - 12;
@@ -92,14 +118,17 @@ public class CoffeeMachine {
                 money = bank - money;
 
                 break;
+
+            case "remaining":
+
+                System.out.println("The coffee machine has: \n" + water + " of water \n" + milk +
+                        " of milk \n" + beans + " of coffee beans \n" + cups + " of disposable cups \n"
+                        + money + " of money");
+
+                break;
+
+
         }
-
-        System.out.println("The coffee machine has: \n" + water + " of water \n" + milk +
-                " of milk \n" + beans + " of coffee beans \n" + cups + " of disposable cups \n"
-                + money + " of money");
-
-//        System.out.println(action);
-
 
     }
 }
